e0ed022133a95507b5f070757517a4fa3dca29b2
infernal-kernel <infernalhacking@gmail.com>
2025-04-08 23:49:03 +0200

    Delete like-button.js

--- like-button.js
+++
@@ -1,7 +0,0 @@
-// Script para manejar la funcionalidad de "Me gusta"
-document.querySelectorAll('.like-btn').forEach(button => {
-    button.addEventListener('click', function() {
-        let currentLikes = parseInt(this.textContent.split(' ')[1]) || 0;
-        this.textContent = `Me gusta ${currentLikes + 1} 👍`;
-    });
-});
